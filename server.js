e7c6f09309aa7bd3e6bf5cd93e27cae3681b4b7e
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2025-11-20 17:45:54 +0400

    //

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.static("public"));
+const PORT = process.env.PORT || 4000;
 
 const client = new MongoClient(process.env.MONGO_URL);
 let reviewsCollection;
@@ -49,4 +50,4 @@ app.get("/reviews", async (req, res) => {
   }
 });
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
