7ac7b3a2a7b1b096ca018f967ccd2aa59a5fe844
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2025-11-20 18:57:44 +0400

    //

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(cors({
+  origin: [
+    "https://smbeautyexpert.netlify.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+
 const PORT = process.env.PORT || 4000;
+
 
 const client = new MongoClient(process.env.MONGO_URL);
 let reviewsCollection;
@@ -51,3 +63,5 @@ app.get("/reviews", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
